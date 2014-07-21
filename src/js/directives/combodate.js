c98cf088e4d15566e08762e01d2211e455f36cc2
Colt McCormack <colt.mccormack@4act.com>
2014-07-21 12:17:35 -0500

    Make sure date gets returned as native Date object, not a string

--- src/js/directives/combodate.js
+++ src/js/directives/combodate.js
@@ -3,14 +3,14 @@ angular.module('xeditable').directive('editableCombodate', ['editableDirectiveFa
   function(editableDirectiveFactory, editableCombodate) {
     return editableDirectiveFactory({
       directiveName: 'editableCombodate',
-      inputTpl: '<input type="text">',
+      inputTpl: '<input type="datetime">',
       render: function() {
         this.parent.render.call(this);
         var combodate = editableCombodate.getInstance(this.inputEl, {value: new Date(this.scope.$data)});
 
         var self = this;
         combodate.$widget.find('select').bind('change', function(e) {
-          self.scope.$data = (new Date(combodate.getValue())).toISOString();
+          self.scope.$data = (new Date(combodate.getValue()));
         });
       }
     });
